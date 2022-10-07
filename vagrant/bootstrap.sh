938c67f5c0a166095f3736d2aa494258902f9c89
ibra4 <ibra16@itc.hu.edu.jo>
2022-10-07 12:00:19 +0000

    init

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# BEGIN ########################################################################
+echo -e "-- ------------------ --\n"
+echo -e "-- BEGIN BOOTSTRAPING --\n"
+echo -e "-- ------------------ --\n"
+
+# VARIABLES ####################################################################
+echo -e "-- Setting global variables\n"
+APACHE_CONFIG=/etc/apache2/apache2.conf
+VIRTUAL_HOST=bana-admin.local
+DOCUMENT_ROOT=/var/www/html/bana-lieter/public
+
+# BOX ##########################################################################
+echo -e "-- Updating packages list\n"
+apt-get update -y -qq
+
+# APACHE #######################################################################
+echo -e "-- Installing Apache web server\n"
+apt-get install -y apache2
+rm -rf /var/www/html/index.html
+
+echo -e "-- Installing php 7.4"
+sudo apt-get install -y php7.4 php7.4-cli php7.4-gd php7.4-curl php7.4-xml php7.4-mbstring libapache2-mod-php7.4
+
+echo -e "-- Adding ServerName to Apache config\n"
+grep -q "ServerName ${VIRTUAL_HOST}" "${APACHE_CONFIG}" || echo "ServerName ${VIRTUAL_HOST}" >> "${APACHE_CONFIG}"
+
+echo -e "-- Allowing Apache override to all\n"
+sed -i "s/AllowOverride None/AllowOverride All/g" ${APACHE_CONFIG}
+
+echo -e "-- Updating vhost file\n"
+cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
+<VirtualHost *:80>
+    ServerName ${VIRTUAL_HOST}
+    DocumentRoot ${DOCUMENT_ROOT}
+
+    <Directory ${DOCUMENT_ROOT}>
+        Options Indexes FollowSymlinks
+        AllowOverride All
+        Order allow,deny
+        Allow from all
+        Require all granted
+    </Directory>
+
+    ErrorLog ${APACHE_LOG_DIR}/${VIRTUAL_HOST}-error.log
+    CustomLog ${APACHE_LOG_DIR}/${VIRTUAL_HOST}-access.log combined
+</VirtualHost>
+EOF
+
+echo -e "-- Enable rewrite mod\n"
+sudo a2enmod rewrite
+
+echo -e "-- Restarting Apache web server\n"
+service apache2 restart
+
+echo -e "-- Installing composer\n"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+# END ##########################################################################
+echo -e "-- ---------------- --"
+echo -e "-- END BOOTSTRAPING --"
+echo -e "-- ---------------- --"
