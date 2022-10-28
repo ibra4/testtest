e85a030931cd79f0bf5300f58d02612b0f5c2691
ibra4 <ibra16@itc.hu.edu.jo>
2022-10-29 00:13:38 +0200

    Add some columns to examinees table

--- bana-lieter/app/Models/Examinee.php
+++ bana-lieter/app/Models/Examinee.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Examinee extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'age',
+        'gender',
+        'country_id',
+        'city_id',
+        'admin_id'
+    ];
 }
