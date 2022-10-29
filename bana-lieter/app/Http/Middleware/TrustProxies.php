0c0c9c78447c53617ea8fa45a02dcb3faa99bc6c
ibra4 <ibra16@itc.hu.edu.jo>
2022-10-29 04:22:08 +0200

    Redirect to https

--- bana-lieter/app/Http/Middleware/TrustProxies.php
+++ bana-lieter/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '38.242.216.5'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
