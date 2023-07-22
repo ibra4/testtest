f795f36a15c271575d1af64fa772b9a5c08cf92f
ibra4 <ibra16@itc.hu.edu.jo>
2023-07-22 16:36:29 +0300

    Update version

--- bana-lieter/resources/views/dashboard.blade.php
+++ bana-lieter/resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
 <body>
     <div id="root"></div>
 
-    <script src="{{ asset('dashboard/dist/main.js') }}?v=11"></script>
+    <script src="{{ asset('dashboard/dist/main.js') }}?v=12"></script>
 </body>
 
 </html>
