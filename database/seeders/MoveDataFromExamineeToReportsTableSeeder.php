8fc171d1f687c872cc3a2e27f03384ccf12e9fca
ibra4 <ibra16@itc.hu.edu.jo>
2023-08-08 20:42:26 +0300

    Fix seeder

--- database/seeders/MoveDataFromExamineeToReportsTableSeeder.php
+++ database/seeders/MoveDataFromExamineeToReportsTableSeeder.php
@@ -16,10 +16,9 @@ class MoveDataFromExamineeToReportsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::beginTransaction();
-
         /** @var Examinee $examinee */
         foreach (Examinee::all() as $examinee) {
+            DB::beginTransaction();
             $date = $examinee->application_date ?? now();
             $report = $examinee->report;
             try {
@@ -29,7 +28,9 @@ class MoveDataFromExamineeToReportsTableSeeder extends Seeder
                     'created_by' => $examinee->created_by,
                 ]);
                 $this->command->info(sprintf('Report %d, updated successfully', $report->id));
+                DB::commit();
             } catch (Throwable $th) {
+                DB::rollBack();
                 $this->command->error(sprintf('Error while updating the report %d, errorMsg: %s', $report->id, $th->getMessage()));
             }
         }
