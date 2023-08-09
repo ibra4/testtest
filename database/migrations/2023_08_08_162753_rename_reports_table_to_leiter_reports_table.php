84887abd55dfb0f40a4dedf18b22eb5ad2d93ef4
ibra4 <ibra16@itc.hu.edu.jo>
2023-08-09 20:34:47 +0300

    Great Job

--- database/migrations/2023_08_08_162753_rename_reports_table_to_leiter_reports_table.php
+++ database/migrations/2023_08_08_162753_rename_reports_table_to_leiter_reports_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class RenameReportsTableToLeiterReportsTable extends Migration
@@ -14,6 +14,21 @@ class RenameReportsTableToLeiterReportsTable extends Migration
     public function up()
     {
         if (Schema::hasTable('reports')) {
+            foreach (DB::table('examinees')->get() as $examinee) {
+                DB::beginTransaction();
+                try {
+                    $date = $examinee->application_date ?? now();
+                    DB::table('reports')->where('id', $examinee->report_id)->update([
+                        'application_date' => $date,
+                        'examinee_id' => $examinee->id,
+                        'created_by' => $examinee->created_by,
+                        'examiner_notes' => $examinee->examiner_notes,
+                    ]);
+                    DB::commit();
+                } catch (Throwable $th) {
+                    DB::rollBack();
+                }
+            }
             Schema::rename('reports', 'leiter_reports');
         }
     }
