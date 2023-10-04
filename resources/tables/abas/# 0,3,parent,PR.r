8a1a7fd6286a070b9173b8b265fae1f637304bb6
ibra4 <ibra16@itc.hu.edu.jo>
2023-10-04 21:30:40 +0300

    Import composite parent 0 to  5.11

---
+++ resources/tables/abas/# 0,3,parent,PR.r
@@ -0,0 +1,16 @@
+# scripts
+mongoimport --db bana --collection abas_composite --type=csv --file resources/tables/abas/composite_parent_0_5_11.csv --uri mongodb://127.0.0.1:27017 --headerline
+
+# 0,3,parent,PR
+# 4,7,parent,PR
+# 8,11,parent,PR
+# 12,15,parent,PR
+# 16,19,parent,PR
+# 20,23,parent,PR
+# 24,29,parent,PR
+# 30,35,parent,PR
+# 36,41,parent,PR
+# 42,47,parent,PR
+# 48,53,parent,PR
+# 54,59,parent,PR
+# 60,71,parent,PR
