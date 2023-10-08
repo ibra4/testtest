f3d76a8881cc9980a6e8baa6780fc38e9b657f70
ibra4 <ibra16@itc.hu.edu.jo>
2023-10-08 21:43:19 +0300

    Abas Composite

--- resources/tables/abas/# 0,3,parent,PR.r
+++ resources/tables/abas/# 0,3,parent,PR.r
@@ -1,16 +1,28 @@
 # scripts
 mongoimport --db bana --collection abas_composite --type=csv --file resources/tables/abas/composite_parent_0_5_11.csv --uri mongodb://127.0.0.1:27017 --headerline
 
-# 0,3,parent,PR
-# 4,7,parent,PR
-# 8,11,parent,PR
-# 12,15,parent,PR
-# 16,19,parent,PR
-# 20,23,parent,PR
-# 24,29,parent,PR
-# 30,35,parent,PR
-# 36,41,parent,PR
-# 42,47,parent,PR
-# 48,53,parent,PR
-# 54,59,parent,PR
-# 60,71,parent,PR
+# 0,3,parent_caregiver,PR
+# 4,7,parent_caregiver,PR
+# 8,11,parent_caregiver,PR
+# 12,15,parent_caregiver,PR
+# 16,19,parent_caregiver,PR
+# 20,23,parent_caregiver,PR
+# 24,29,parent_caregiver,PR
+# 30,35,parent_caregiver,PR
+# 36,41,parent_caregiver,PR
+# 42,47,parent_caregiver,PR
+# 48,53,parent_caregiver,PR
+# 54,59,parent_caregiver,PR
+# 60,71,parent_caregiver,PR
+
+# ,60,71,parent,PR
+# ,72,83,parent,PR
+# ,84,95,parent,PR
+# ,96,107,parent,PR
+# ,108,119,parent,PR
+# ,120,131,parent,PR
+# ,132,143,parent,PR
+# ,144,155,parent,PR
+# ,156,179,parent,PR
+# ,180,203,parent,PR
+# ,204,263,parent,PR
