8a6113252e897f0a66e8aead0133d00a1563103a
ibra4 <ibra16@itc.hu.edu.jo>
2023-10-11 21:23:25 +0300

    Update tables

--- resources/tables/abas/# 0,3,parent,PR.r
+++ resources/tables/abas/# 0,3,parent,PR.r
@@ -15,10 +15,22 @@ mongoimport --db bana --collection abas_composite --type=csv --file resources/ta
 # 54,59,parent_caregiver,PR
 # 60,71,parent_caregiver,PR
 
-# ,24,29,teacher_caregiver,PR
-# ,30,35,teacher_caregiver,PR
-# ,36,41,teacher_caregiver,PR
-# ,42,47,teacher_caregiver,PR
-# ,48,53,teacher_caregiver,PR
-# ,54,59,teacher_caregiver,PR
-# ,60,71,teacher_caregiver,PR
+# ,60,71,teacher,PR
+# ,72,83,teacher,PR
+# ,84,95,teacher,PR
+# ,96,107,teacher,PR
+# ,108,119,teacher,PR
+# ,120,131,teacher,PR
+# ,132,143,teacher,PR
+# ,144,155,teacher,PR
+# ,156,179,teacher,PR
+# ,180,203,teacher,PR
+# ,204,263,teacher,PR
+
+# ,192,263,adult_by_self,SO
+# ,264,359,adult_by_self,SO
+# ,360,479,adult_by_self,SO
+# ,480,599,adult_by_self,SO
+# ,600,779,adult_by_self,SO
+# ,780,899,adult_by_self,SO
+# ,900,1079,adult_by_self,SO
