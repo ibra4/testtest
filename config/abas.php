660b95fcafa5f598ae473f765fd0242b1da02f0b
ibra4 <ibra16@itc.hu.edu.jo>
2023-10-15 21:01:12 +0300

    Update abas config

--- config/abas.php
+++ config/abas.php
@@ -10,6 +10,41 @@ return [
                 'name' => 'المركب التكيفي العام (GAC)',
                 'sum_of' => ['com', 'cu', 'fa', 'sl', 'hs', 'ls', 'sc', 'sd', 'soc', 'wk'],
             ]
+        ],
+        AbasExamTypesEnum::TEACHER_CAREGIVER => [
+            [
+                'code' => 'GAC',
+                'name' => 'المركب التكيفي العام (GAC)',
+                'sum_of' => ['com', 'fa', 'sl', 'hs', 'ls', 'sc', 'sd', 'soc', 'mo'],
+            ]
+        ],
+        AbasExamTypesEnum::PARENT => [
+            [
+                'code' => 'GAC',
+                'name' => 'المركب التكيفي العام (GAC)',
+                'sum_of' => ['com', 'cu', 'fa', 'hl', 'ls', 'ls', 'sc', 'sd', 'soc', 'wk'],
+            ]
+        ],
+        AbasExamTypesEnum::PARENT_CAREGIVER => [
+            [
+                'code' => 'GAC',
+                'name' => 'المركب التكيفي العام (GAC)',
+                'sum_of' => ['com', 'cu', 'fa', 'hl', 'hs', 'ls', 'sc', 'sd', 'soc', 'mo'],
+            ]
+        ],
+        AbasExamTypesEnum::ADULT_BY_OTHERS => [
+            [
+                'code' => 'GAC',
+                'name' => 'المركب التكيفي العام (GAC)',
+                'sum_of' => [],
+            ]
+        ],
+        AbasExamTypesEnum::ADULT_BY_SELF => [
+            [
+                'code' => 'GAC',
+                'name' => 'المركب التكيفي العام (GAC)',
+                'sum_of' => [],
+            ]
         ]
     ]
 ];
