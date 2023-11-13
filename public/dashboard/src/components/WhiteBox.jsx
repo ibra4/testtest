3982e1f8d053b9b24628eb8687d532066b634a7d
ibra4 <ibra16@itc.hu.edu.jo>
2023-11-13 18:15:35 +0300

    Restore shadow

--- public/dashboard/src/components/WhiteBox.jsx
+++ public/dashboard/src/components/WhiteBox.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 function WhiteBox({ children, title, classes, shadow = true, hr = false, ...rest }) {
     return (
-        <div className={classNames('white-box', classes, shadow ?? 'shadow')} {...rest}>
+        <div className={classNames('white-box', classes, shadow ? 'shadow' : '')} {...rest}>
             {title && <div className="title">{title}</div>}
             {hr && <hr />}
             {children}
